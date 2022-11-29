8378af1a617ad65239b5097b73fc5cade6d27ef7
siavnraj <boyinasivanagaraju5@gmail.com>
2022-11-29 15:43:34 +0530

    first commit

---
+++ ex.1.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+dataset="dataset"
+name="hamp"
+
+path=os.path.join(dataset,name)
+if not os.path.isdir(path):
+    os.mkdir(path)
+
+(width,height)=(130,100)
+
+
+alg="haarcascade_frontalface_default.xml"
+haar_cascade=cv2.CascadeClassifier(alg)
+cam=cv2.VideoCapture(0)
+
+
+count=1
+while count<31:
+    print(count)
+
+    _,img=cam.read()
+    grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face=haar_cascade.detectMultiScale(grayimg,1.3,4)
+    for (x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        faceonly=grayimg[y:y+h,x:x+w]
+        resize=cv2.resize(faceonly,(width,height))
+        cv2.imwrite("%s/%s.jpg" % (path,count),faceonly)
+        count+=1
+    cv2.imshow("facedetection",img)
+    key=cv2.waitKey(50)
+    if key ==27: 
+        break
+print("person detected successfully")   
+cam.release()
+cv2.destroyAllWindows()
+    
+     
